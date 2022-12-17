83150a0e8c5be005b8852737b6d512ac4dee2091
Ben Baptist <sawham6@gmail.com>
2022-12-16 20:26:27 -0500

    removed build

--- build/lib/wrapper/__version__.py
+++
@@ -1,5 +0,0 @@
-VERSION = (0, 1, 13)
-TYPE = "alpha"
-
-__version__ = '.'.join(map(str, VERSION))
-__version__ += TYPE
--- build/lib/wrapper/exceptions.py
+++
@@ -1,5 +0,0 @@
-class StartingException(Exception): pass
-class ServerStopped(Exception): pass
-class ConsoleError(Exception): pass
-class UnsupportedFormat(Exception): pass
-class AuthError(Exception): pass
