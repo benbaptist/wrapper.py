b3b100b5601002b6bfc77bdb5a3d1cf704d16206
Ben Baptist <sawham6@mgail.com>
2026-01-20 19:23:44 -0500

    clarifying purpose of start-wraperπy

--- start-wrapper.py
+++ start-wrapper.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 from wrapper import Wrapper
 
+# Start script for development purposes
+# For non-development purposes, use `mcwrapper` 
+# command after installation
+
 if __name__ == "__main__":
     wrapper = Wrapper()
     wrapper.start()
